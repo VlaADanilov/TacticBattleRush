045a4190e4f9971d8a0fd723fd2c646a5e098025
VlaADanilov <helloampro@gmail.com>
2025-01-14 10:41:07 +0300

    Производительность

--- Client/src/main/java/org/example/client/controllers/RezultController.java
+++ Client/src/main/java/org/example/client/controllers/RezultController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import org.example.client.HelloApplication;
 import org.example.client.util.Images;
 import org.example.client.util.WhoWinner;
@@ -16,6 +17,11 @@ public class RezultController {
 
 
     public void initialize() {
+
+        rezultImage.setPreserveRatio(false); // Сохранять пропорции
+        rezultImage.setSmooth(true);  // Сглаживание изображения
+        rezultImage.setCache(true); // Кэширование изображения
+
         rezultImage.setImage(Images.getRezultImage(WhoWinner.getWinner()));
     }
 
